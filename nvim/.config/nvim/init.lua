0c926a68cac26c04ad493bb037c77e4fe0253eaa
HieuBNT <hieu.buinguyen2352001@gmail.com>
2025-10-05 16:42:16 +0700

    Change hightlight and cursor color

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_set_hl(0, "Visual", { bg = "#FFD700", fg = "NONE" })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#e7e7e7", fg = "NONE" })
