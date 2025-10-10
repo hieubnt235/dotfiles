b2c0d9cf7a3d738840653335afa226fcf6f2ca84
HieuBNT <hieu.buinguyen2352001@gmail.com>
2025-10-10 10:28:48 +0700

    toggleterm

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,46 @@
 return {
     {
-        "rmehri01/onenord.nvim",
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
+    },
+
+    {
+        "catppuccin/nvim",
+        -- name = "catppuccin",
         lazy = false,
         priority = 1000,
         opts = {
-            theme = "light",
+            flavour = "latte", -- latte, frappe, macchiato, mocha
             styles = {
-                comments = "italic",
-                strings = "None",
-                keywords = "bold",
-                functions = "bold",
-                variables = "NONE",
-                diagnostics = "underline",
+                loops = { "italic" },
+                functions = { "bold" },
+                keywords = { "bold" },
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = { "bold" },
+                operators = {},
             },
         },
     },
+    -- {
+    --     "rmehri01/onenord.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {
+    --         theme = "light",
+    --         styles = {
+    --             comments = "italic",
+    --             strings = "None",
+    --             keywords = "bold",
+    --             functions = "bold",
+    --             variables = "NONE",
+    --             diagnostics = "underline",
+    --         },
+    --     },
+    -- },
 }
--- nvim/.config/nvim/lua/plugins/cpp.lua
+++ nvim/.config/nvim/lua/plugins/cpp.lua
@@ -1,3 +1,23 @@
 return {
     { "Civitasv/cmake-tools.nvim", enabled = false },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                -- Ensure mason installs the server
+                clangd = {
+                    cmd = {
+                        "clangd",
+                        "--background-index",
+                        "--clang-tidy",
+                        "--header-insertion=iwyu",
+                        "--completion-style=detailed",
+                        "--function-arg-placeholders",
+                        "--fallback-style=llvm",
+                        "--experimental-modules-support",
+                    },
+                },
+            },
+        },
+    },
 }
