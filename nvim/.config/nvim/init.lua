6d98119d47e4e810ed6ef419ed0cdba75a2e4d4a
HieuBNT235 <hieu.buinguyen2352001@gmail.com>
2025-10-28 11:17:38 +0700

    change theme to catppuccin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,5 @@ require("config.lazy")
 vim.api.nvim_set_hl(0, "Visual", { bg = "#FFD700", fg = "NONE" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#e7e7e7", fg = "NONE" })
 
--- vim.opt.shell = "/bin/bash"
 vim.opt.clipboard = "unnamedplus"
+-- vim.opt.shell = "/bin/bash"
--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,7 +1,9 @@
 --https://github.com/LazyVim/LazyVim/discussions/4291
 return {
-    "mfussenegger/nvim-dap-python",
-    config = function()
-        require("dap-python").setup("path/to/debugpy")
-    end,
+    {
+        "mfussenegger/nvim-dap-python",
+        config = function()
+            require("dap-python").setup("path/to/debugpy")
+        end,
+    },
 }
