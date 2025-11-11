c13caaf453d45692278ea19421531a62d837bb3d
hieu <hieu@192.168.1.6.non-exists.ptr.local>
2025-11-11 23:04:42 +0700

    config fish

---
+++ fish/.config/fish/conf.d/bobthefish.fish
@@ -0,0 +1,6 @@
+set -g theme_color_scheme base16-light
+set -g theme_display_hostname yes
+set -g theme_display_user yes
+set -g theme_display_sudo_user yes
+set -g theme_newline_cursor yes
+set -g theme_nerd_fonts yes
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -g theme_color_scheme base16-light
+
 set -x PATH $PATH ~/.local/bin/
