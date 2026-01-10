2e38869ef4a902ce39755e3470b51740877729d0
HieuBNT <hieu.buinguyen2352001@gmail.com>
2026-01-10 10:12:48 +0700

    change to default font

--- nvim/.config/nvim/lua/plugins/xml.lua
+++ nvim/.config/nvim/lua/plugins/xml.lua
@@ -16,6 +16,22 @@ return {
             formatters_by_ft = {
                 xml = { "xmlformatter" },
             },
+
+            formatters = {
+                xmlformatter = {
+
+                    -- xmlformat [--preserve "pre,literal"] [--blanks] [--compress] [--selfclose] [--indent num] [--indent-char char]
+                    --        [--overwrite] [--outfile file] [--encoding enc] [--outencoding enc] [--disable-inlineformatting]
+                    --        [--disable-correction] [--preserve-attributes] [--encode-attributes] [--help] < --infile file | file | - >
+                    -- prepend_args ensures these flags are injected before the defaults
+                    prepend_args = {
+                        "--indent",
+                        "4",
+                        "--blanks",
+                        "--preserve-attributes",
+                    },
+                },
+            },
         },
     },
 }
