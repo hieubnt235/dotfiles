a47e79bd242ff2e1e185324185f62065c8d2559f
HieuBNT <hieu.buinguyen2352001@gmail.com>
2026-01-26 15:28:06 +0700

    change colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -65,6 +65,9 @@ return {
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
+            local namespace_color = "darkcyan"
+            local class_color = "lightseagreen"
+            local directive_color = "dodgerblue"
             require("github-theme").setup({
                 options = {
                     styles = {
@@ -74,9 +77,38 @@ return {
                         -- types = "italic,bold",
                     },
                 },
+                groups = {
+                    github_light_default = {
+
+                        -- Targets the 'ns' in 'ns::Member'
+                        ["@module"] = { fg = namespace_color },
+                        -- Targets 'ns' specifically in C++/Rust scope resolution
+                        ["@namespace"] = { fg = namespace_color },
+                        -- Optional: If you want the '::' to be a specific color
+                        ["@punctuation.delimiter"] = { fg = namespace_color },
+
+                        ["@keyword"] = { fg = "blue" },
+                        ["@type.builtin"] = { fg = "blue" },
+                        ["@keyword.directive"] = { fg = directive_color },
+                        ["@keyword.import"] = { fg = directive_color },
+                        -- ["@keyword.vim"] = { fg = "indianred" },
+                        --
+                        -- -- Targets 'class', 'struct', 'enum'
+                        -- ["@keyword.type"] = { fg = "blue" },
+                        -- -- Targets 'auto'
+                        -- ["@keyword.modifier"] = { fg = "blue" },
+                    },
+                },
                 specs = {
                     github_light_default = {
                         syntax = {
+                            -- keyword = "blue",
+                            builtin0 = "blue",
+                            builtin1 = "blue",
+                            builtin2 = "blue",
+                            conditional = "blue",
+                            preproc = directive_color,
+                            type = class_color,
                             string = "red",
                             comment = "green",
                         },
